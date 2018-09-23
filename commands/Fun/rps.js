6e72dc8f3ea43a258c6bc5fed68a3e9bd45dbaea
James <thiagozevedo3@gmail.com>
2018-09-23 16:20:25 -0300

    fixes

--- commands/Fun/rps.js
+++ commands/Fun/rps.js
@@ -3,46 +3,48 @@ const choices = ['rock', 'paper', 'scissors'];
 
 module.exports = class extends Command {
 
-	constructor(...args) {
-		super(...args, {
-			description: 'Play a game of rock, paper, scissors with the bot.',
-			usage: '<rock|r|paper|p|scissor|s>'
-		});
-	}
+    constructor(...args) {
+        super(...args, {
+            description: 'Play a game of rock, paper, scissors with the bot.',
+            usage: `<rock|r|paper|p|scissors|s>`
+        });
+    }
 
-	async run(message, [action]) {
+    async run(message, [action]) {
         const outcome = choices[Math.floor(Math.random() * choices.length)];
         const choice = action.toLowerCase();
-	        switch (action) {
+        switch (action) {
             case 'rock':
                 switch (outcome) {
                     case 'rock':
                         return message.sendMessage('***Rock! That\'s a tie!***');
                     case 'paper':
                         return message.sendMessage('***Paper! I win, you loose!***');
-                    case 'scissor':
+                    case 'scissors':
                         return message.sendMessage('***Scissors! No! You won...***');
                 };
                 break;
             case 'paper':
                 switch (outcome) {
                     case 'rock':
-                        return message.sendMessage('***Rock! That\'s a tie!***');
+                        return message.sendMessage('***Rock! No! You won...***');
                     case 'paper':
-                        return message.sendMessage('***Paper! I win, you loose!***');
-                    case 'scissor':
-                        return message.sendMessage('***Scissors! No! You won...***');
+                        return message.sendMessage('***Paper! That\'s a tie!***');
+                    case 'scissors':
+                        return message.sendMessage('***Scissors! I win, you loose...***');
                 }
+                break;
             case 'scissors':
                 switch (outcome) {
                     case 'rock':
-                        return message.sendMessage('***Rock! That\'s a tie!***');
+                        return message.sendMessage('***Rock! I win, you loose!!***');
                     case 'paper':
-                        return message.sendMessage('***Paper! I win, you loose!***');
-                    case 'scissor':
-                        return message.sendMessage('***Scissors! No! You won...***');
+                        return message.sendMessage('***Paper! No! You won...***');
+                    case 'scissors':
+                        return message.sendMessage('***Scissors! That\'s a tie!***');
                 }
+                break;
         }
-	}
+    }
 
 };
