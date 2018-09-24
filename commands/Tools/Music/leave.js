e904d65022e96a62dc9fc0310f8e7ecc43d562db
Skillz <gankstarsleague@gmail.com>
2018-09-23 21:57:50 -0400

    leave a voice channelc command

---
+++ commands/Tools/Music/leave.js
@@ -0,0 +1,23 @@
+const { Command } = require('klasa');
+
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			runIn: ['text'],
+			permissionLevel: 0,
+			description: 'Make the bot leave a voice channel.',
+		});
+	}
+
+	async run(message) {
+		const voiceChannel = message.guild.me.voice.channel;
+
+		if (!voiceChannel) throw `I can not leave a channel if I am not in one silly.`
+
+		voiceChannel.leave();
+
+		return message.sendMessage(`Successfully left the **${voiceChannel.name}** channel.`);
+	}
+
+};
