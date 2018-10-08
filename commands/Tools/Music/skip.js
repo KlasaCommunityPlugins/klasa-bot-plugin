997ac96ce2e679d4d6fa46ce95dc28fef9bdc686
Skillz <gankstarsleague@gmail.com>
2018-10-08 16:37:03 -0400

    cleanup of skip

--- commands/Tools/Music/skip.js
+++ commands/Tools/Music/skip.js
@@ -1,35 +1,14 @@
-const { Command } = require('klasa');
-const ytdl = require('ytdl-core');
+const { Inhibitor } = require('klasa');
 
-module.exports = class extends Command {
+module.exports = class extends Inhibitor {
 
-	constructor(...args) {
-		super(...args, {
-			runIn: ['text'],
-			description: 'Make the bot join a voice channel.',
-			usage: '[amount:int{1}]',
-		});
-	}
+	async run(message, command) {
+		if (!command.requiredSettings.length) return;
 
-	async run(message, [amount = 1]) {
-		const { playlist } = message.guild.settings.playlist;
-
-		if (amount > playlist.length) throw message.sendMessage(`You can't skip more songs than currently in the playlist, silly!`);
-
-		const remainingSongsInPlaylist = playlist.splice(amount);
-
-		const { errors } = await message.guild.settings.update('music.playlist', remainingSongsInPlaylist, { action: 'overwrite' });
-
-		if (errors.length) {
-			this.client.emit('error', errors.join('\n'));
-			return message.sendMessage('There was an error with updating the playlist settings. Please try again later.');
-		}
-
-		const nextSongInPlaylist = remainingSongsInPlaylist[0];
-		const playingNow = await joinedVoiceChannel.play(ytdl(nextSongInPlaylist, { filter: 'audioonly' }));
-		if (!playingNow) return message.sendMessage(`Unable to play the song ${nextSongInPlaylist}.`);
-
-		return message.sendMessage(`${amount} songs have been skipped in the playlist and now playing ${nextSongInPlaylist}`);
+		const isGuildChannel = message.channel.type === 'text';
+		// eslint-disable-next-line eqeqeq
+		const requiredSettings = command.requiredSettings.filter(setting => message[isGuildChannel ? 'guild' : 'author'].settings.get(setting) == null);
+		if (requiredSettings.length) throw message.language.get('INHIBITOR_REQUIRED_SETTINGS', requiredSettings);
 	}
 
 };
