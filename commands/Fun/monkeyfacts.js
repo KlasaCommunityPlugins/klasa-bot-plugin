2973952f2c1fecea1d246f435a373455ecef5101
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2018-11-13 16:11:47 -0500

    Create monkeyfacts.js

---
+++ commands/Fun/monkeyfacts.js
@@ -0,0 +1,16 @@
+const { Command } = require('klasa');
+const { array: { monkeyfacts } } = require('../../array.js')
+module.exports = class extends Command {
+
+	constructor(...args) {
+		super(...args, {
+			aliases: ['mf'],
+			description: 'Gives you a random monkey fact',
+		});
+	}
+
+	run(message) {
+        return message.sendMessage(facts[Math.floor(Math.random() * monkeyfacts.length)]);
+	}
+
+};
