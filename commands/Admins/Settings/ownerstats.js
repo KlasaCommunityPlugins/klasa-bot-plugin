afd78bff5c80a7404d67228f23965d3cfc6bc29d
Skillz4Killz <23035000+Skillz4Killz@users.noreply.github.com>
2018-11-16 18:03:25 -0500

    Update commands/Admins/Settings/ownerstats.js
    Co-Authored-By: WallyThePenguin <32379213+WallyThePenguin@users.noreply.github.com>

--- commands/Admins/Settings/ownerstats.js
+++ commands/Admins/Settings/ownerstats.js
@@ -25,6 +25,17 @@ module.exports = class extends Command {
 		.addField(`Klasa Version:`,klasaVersion,true)
 		.addField(`Discord.js Version:`, discordVersion,true)
 		.addField(`Node.js Version`, process.version,true)
-		message.send(embed1)
+		message.send(new MessageEmbed()
+		.setAuthor(this.client.user.username, this.client.user.displayAvatarURL())
+		.setColor(7415665)
+		.addField(`Memory Usage:`,  (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2),true)
+		.addField(`Uptime:`, Duration.toNow(Date.now() - (process.uptime() * 1000)),true)
+		.addField(`Server count:`, `${this.client.guilds.size}`,true)
+		.addField(`Player/User count:`,` ${this.client.users.size}`,true)
+		.addField(`Owner:`, (this.client.owner),true)
+		.addField(`Default Prefix:`,(prefix),true)
+		.addField(`Klasa Version:`,klasaVersion,true)
+		.addField(`Discord.js Version:`, discordVersion,true)
+		.addField(`Node.js Version`, process.version,true))
     }
 }
