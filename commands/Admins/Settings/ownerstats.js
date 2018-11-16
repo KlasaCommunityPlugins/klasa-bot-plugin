633eb07bc81d4a8a93f03de67dd13459357747a5
Waleed <32379213+WallyThePenguin@users.noreply.github.com>
2018-11-16 18:10:41 -0500

    Update ownerstats.js

--- commands/Admins/Settings/ownerstats.js
+++ commands/Admins/Settings/ownerstats.js
@@ -5,7 +5,7 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			description: 'See the bots private stats that users don't need.',
+			description: 'See the bots private stats that users don\'t need.',
 			permissionLevel: 10,
 			aliases: ['os']
 		});
