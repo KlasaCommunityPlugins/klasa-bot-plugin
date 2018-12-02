6463d50b0ca71096cd6156510c3cef926e90341e
Skillz <gankstarsleague@gmail.com>
2018-12-02 15:29:33 -0500

    adding a better error response when missing mute role in settings

--- commands/Moderation/mute.js
+++ commands/Moderation/mute.js
@@ -25,10 +25,6 @@ SOFTWARE.
 
 const { Command, Duration } = require('klasa');
 
-// Add to your schema definition:
-// KlasaClient.defaultGuildSchema.add('roles', schema => schema
-//   .add('muted', 'role'));
-
 module.exports = class extends Command {
 
 	constructor(...args) {
@@ -45,23 +41,16 @@ module.exports = class extends Command {
 	async run(message, [when, member, ...reason]) {
 		if (member.id === message.author.id) throw 'Why would you mute yourself?';
 		if (member.id === this.client.user.id) throw 'Have I done something wrong?';
-
 		if (member.roles.highest.position >= message.member.roles.highest.position) throw 'You cannot mute this user.';
-
 		if (member.roles.has(message.guild.settings.roles.muted)) throw 'The member is already muted.';
+		if (!message.guild.settings.roles.muted) throw `You did not set a role to be used as the muted role. Please type **${message.guild.settings.prefix}conf set roles.muted @role** to set a role to be used to mute users on your server.`;
+
 		await member.roles.add(message.guild.settings.roles.muted);
 
-		if (when) {
-			await this.client.schedule.create('unmute', when, {
-				data: {
-					guild: message.guild.id,
-					user: member.id
-				}
-			});
-			return message.sendMessage(`${member.user.tag} got temporarily muted for ${Duration.toNow(when)}.${reason ? ` With reason of: ${reason}` : ''}`);
-		}
+		if (!when) return message.sendMessage(`${member.user.tag} got muted.${reason.length ? ` With reason of: ${reason.join(' ')}` : ''}`);
 
-		return message.sendMessage(`${member.user.tag} got muted.${reason ? ` With reason of: ${reason}` : ''}`);
+		await this.client.schedule.create('unmute', when, { data: { guild: message.guild.id, user: member.id } });
+		return message.sendMessage(`${member.user.tag} got temporarily muted for ${Duration.toNow(when)}.${reason ? ` With reason of: ${reason}` : ''}`);
 	}
 
 };
